38a84f9f0ab4d37c9e5230fdf201a9849eb16147
Isaac Weisberg <isaac_weisberg@yahoo.com>
2018-05-30 10:49:01 +0700

    Add first implementation of request with things

--- Sources/SnatchTwo/Snatch.swift
+++ Sources/SnatchTwo/Snatch.swift
@@ -5,4 +5,34 @@
 //  Created by Isaac Weisberg on 5/30/18.
 //
 
-extension
+import Foundation
+import RxSwift
+import SnatchBase
+
+public extension Snatch {
+    public func request(_ request: URLRequest) -> Observable<Result> {
+        return Observable.create { observer in
+            let task = self.task(with: request, createHandler(observer))
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+}
+
+internal func createHandler(_ observer: AnyObserver<Result>) -> Snatch.DataTaskCallback {
+    return { data, response, error in
+        if let error = error {
+            observer.onError(error)
+        }
+        if let response = response as? HTTPURLResponse {
+            let packedResponse = Result(from: response, data)
+            observer.onNext(packedResponse)
+        } else {
+            observer.onError(SnatchError.spooks)
+        }
+        observer.onCompleted()
+    }
+}
